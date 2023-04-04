477f253226267cba13357190a357882ff6433ed9
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2023-04-04 11:29:22 -0700

    Rework crate list and generate workflows via a script

---
+++ scripts/generate.sh
@@ -0,0 +1,30 @@
+#!/bin/env bash
+
+set -e
+
+cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.."
+
+rm .github/workflows/*
+
+for crate in $(cat crates.txt); do
+  cat > ".github/workflows/$crate.yml" <<EOF
+name: $crate
+
+on:
+  push:
+    branches:
+      - $crate
+  workflow_dispatch:
+
+jobs:
+  build:
+    uses: ./.github/workflows/build.yml
+    with:
+      crate: $crate
+    permissions:
+      contents: write
+    secrets: inherit
+EOF
+done
+
+cp -f .github/staging/* .github/workflows
