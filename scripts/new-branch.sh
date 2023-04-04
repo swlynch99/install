544f9c787f7ec7e81263a96f6bb9a6baa5477fac
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2023-04-04 13:43:16 -0700

    Add a script to generate a new branch for a crate

---
+++ scripts/new-branch.sh
@@ -0,0 +1,38 @@
+#!/bin/env bash
+
+set -e
+cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.."
+
+if ! git diff-index --quiet HEAD; then
+    echo "Not running rebase.sh because git working directory is dirty" >&2
+    exit 1
+fi
+
+if [ -z "$1" ]; then
+    echo "Usage: scripts/new-branch.sh [crate]" >&2
+    exit 1
+fi
+
+crate="$1"
+
+git checkout master
+git checkout -b "$crate"
+
+cat > "$crate.patch" <<EOF
+diff --git a/action.yml b/action.yml
+--- a/action.yml
++++ b/action.yml
+@@ -5,6 +5,7 @@ inputs:
+   crate:
+     description: Name of crate as published to crates.io
+     required: true
++    default: $crate
+   bin:
+     description: Name of binary; default = same as crate name
+     required: false
+EOF
+
+git apply "$crate.patch"
+rm "$crate.patch"
+
+git push -u origin "$crate"
